22337c99b602601bf4e9876045fe4e26045b97e6
Christian Femers <christian.femers@t-online.de>
2022-04-17 17:56:17 +0200

    Refactor method negation pushdown, add variants and java.util.Objects

--- plugins/net.maisikoleni.eclipse.extensions.jdt.cleanup.plugin/src/net/maisikoleni/eclipse/extensions/jdt/cleanup/negation/MethodNegationPushdownCandidate.java
+++ plugins/net.maisikoleni.eclipse.extensions.jdt.cleanup.plugin/src/net/maisikoleni/eclipse/extensions/jdt/cleanup/negation/MethodNegationPushdownCandidate.java
@@ -1,8 +1,12 @@
 package net.maisikoleni.eclipse.extensions.jdt.cleanup.negation;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.IMethodBinding;
@@ -18,22 +22,42 @@ import net.maisikoleni.eclipse.extensions.jdt.cleanup.SimpleCleanUp;
 
 @SuppressWarnings("restriction")
 public enum MethodNegationPushdownCandidate {
-	OPTIONAL("java.util.Optional", "isPresent", "isEmpty"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	STREAM("java.util.stream.Stream", "anyMatch", "noneMatch", "java.util.function.Predicate"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	OBJECTS_NULL("nonNull", "isNull", "java.util.Objects", "java.lang.Object"),
+	OPTIONAL("isPresent", "isEmpty", Set.of( //
+			"java.util.Optional", //
+			"java.util.OptionalInt", //
+			"java.util.OptionalLong", //
+			"java.util.OptionalDouble" //
+	)),
+	STREAM("anyMatch", "noneMatch", //
+			new Variant("java.util.stream.Stream", "java.util.function.Predicate"), //
+			new Variant("java.util.stream.IntStream", "java.util.function.IntPredicate"), //
+			new Variant("java.util.stream.LongStream", "java.util.function.LongPredicate"), //
+			new Variant("java.util.stream.DoubleStream", "java.util.function.DoublePredicate") //
+	);
 
 	private final String id;
-	private final String ownerName;
+	private final List<Variant> variants;
 	private final String methodNameA;
 	private final String methodNameB;
-	private final String[] parameterTypeNames;
 
-	MethodNegationPushdownCandidate(String ownerName, String methodNameA, String methodNameB,
-			String... parameterTypeNames) {
+	MethodNegationPushdownCandidate(String methodNameA, String methodNameB, Variant... variants) {
 		this.id = CleanUpExtensions.METHOD_NEGATION_PUSHDOWN + "." + name().toLowerCase(Locale.ROOT);
-		this.ownerName = ownerName;
-		this.methodNameA = methodNameA;
-		this.methodNameB = methodNameB;
-		this.parameterTypeNames = parameterTypeNames;
+		this.variants = List.of(variants);
+		if (this.variants.isEmpty())
+			throw new IllegalArgumentException("Variants may not be empty");
+		this.methodNameA = Objects.requireNonNull(methodNameA);
+		this.methodNameB = Objects.requireNonNull(methodNameB);
+	}
+
+	MethodNegationPushdownCandidate(String methodNameA, String methodNameB, Set<String> types,
+			String... parameterTypeNames) {
+		this(methodNameA, methodNameB,
+				types.stream().map(type -> new Variant(type, parameterTypeNames)).toArray(Variant[]::new));
+	}
+
+	MethodNegationPushdownCandidate(String methodNameA, String methodNameB, String type, String... parameterTypeNames) {
+		this(methodNameA, methodNameB, Set.of(type), parameterTypeNames);
 	}
 
 	public String getId() {
@@ -42,12 +66,30 @@ public enum MethodNegationPushdownCandidate {
 
 	@Override
 	public String toString() {
-		return ownerName + ": " + methodNameA + " <---> " + methodNameB;
+		var result = new StringBuilder();
+		result.append(methodNameA);
+		result.append(" <---> ");
+		result.append(methodNameB);
+		result.append(" for the type ");
+		result.append(variants.get(0).type());
+		if (variants.size() > 1) {
+			result.append(" (and ");
+			result.append(variants.size() - 1);
+			result.append(" others)");
+		}
+		return result.toString();
 	}
 
 	boolean appliesTo(IMethodBinding methodBinding) {
-		return ASTNodes.usesGivenSignature(methodBinding, ownerName, methodNameA, parameterTypeNames)
-				|| ASTNodes.usesGivenSignature(methodBinding, ownerName, methodNameB, parameterTypeNames);
+		var name = methodBinding.getName();
+		if (!name.equals(methodNameA) && !name.equals(methodNameB))
+			return false;
+		/*
+		 * Now we know the name matches one of both possible ones, so just check the
+		 * type and rest of the signature.
+		 */
+		return variants.stream().anyMatch(
+				variant -> ASTNodes.usesGivenSignature(methodBinding, variant.type(), name, variant.parameters()));
 	}
 
 	TextEditGroup rewriteToCounterpart(ASTRewrite rewrite, AST ast, MethodInvocation methodInvocation,
@@ -62,19 +104,18 @@ public enum MethodNegationPushdownCandidate {
 	}
 
 	public String getPreview(SimpleCleanUp cleanUp) {
-		boolean enabled = cleanUp.isEnabled(getId());
-		switch (this) {
-			case OPTIONAL:
-				if (enabled)
-					return "boolean empty = Optional.of(1).isEmpty();";
-				return "boolean empty = !Optional.of(1).isPresent();";
-			case STREAM:
-				if (enabled)
-					return "boolean anyMatch = Stream.of(2).anyMatch(x -> x > 0);";
-				return "boolean anyMatch = !Stream.of(2).noneMatch(x -> x > 0);";
-			default:
-				throw new IllegalStateException(name());
-		}
+		boolean disabled = !cleanUp.isEnabled(getId());
+		return switch (this) {
+			case OPTIONAL -> disabled ? //
+					"boolean empty = !Optional.of(1).isPresent();" : //
+					"boolean empty = Optional.of(1).isEmpty();";
+			case STREAM -> disabled ? //
+					"boolean anyMatch = !Stream.of(2).noneMatch(x -> x > 0);" : //
+					"boolean anyMatch = Stream.of(2).anyMatch(x -> x > 0);";
+			case OBJECTS_NULL -> disabled ? //
+					"boolean isNull = !Objects.nonNull(3);" : //
+					"boolean isNull = Objects.isNull(3);";
+		};
 	}
 
 	public static void selectOnly(CleanUpOptions options, MethodNegationPushdownCandidate... candidates) {
@@ -85,5 +126,31 @@ public enum MethodNegationPushdownCandidate {
 		for (var candidate : values())
 			options.setOption(candidate.getId(),
 					candidates.contains(candidate) ? CleanUpOptions.TRUE : CleanUpOptions.FALSE);
+	}
+
+	record Variant(String type, String... parameters /* using array here because of JDT API */) {
+
+		Variant {
+			Objects.requireNonNull(type);
+			Stream.of(parameters).forEach(Objects::requireNonNull);
+		}
+
+		@Override
+		public String toString() {
+			return type + "(" + String.join(",", parameters) + ")";
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(type) * 31 + Arrays.hashCode(parameters);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return this == obj || (obj instanceof Variant other) //
+					&& Arrays.equals(parameters, other.parameters) //
+					&& Objects.equals(type, other.type);
+		}
+
 	}
 }
